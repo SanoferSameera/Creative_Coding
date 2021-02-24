5a8ebcd71f5681ceb6f65b620511b5bb6868ea4a
SanoferSameera <79590907+SanoferSameera@users.noreply.github.com>
2021-02-24 21:40:35 +0530

    Add files via upload

---
+++ Fourballs/src/main/java/FourBall.java
@@ -0,0 +1,43 @@
+import processing.core.PApplet;
+public class FourBall extends PApplet{
+
+    public static final int WIDTH = 648;
+    public static final int HEIGHT = 448;
+    public static final int DIAMETER = 10;
+    int w = 0;
+    int x= 0;
+    int y= 0;
+    int z = 0;
+    public static void main(String[] args) {
+        PApplet.main("FourBall",args);
+    }
+
+
+    @Override
+    public void settings() {
+        super.settings();
+        size(WIDTH, HEIGHT);
+    }
+
+    @Override
+    public void setup() {
+    }
+
+    @Override
+    public void draw() {
+        ellipse(w, HEIGHT/5, DIAMETER, DIAMETER);
+        w = w+1;
+        ellipse(x, HEIGHT*2/5, DIAMETER, DIAMETER);
+        x = x+2;
+        ellipse(y, HEIGHT*3/5, DIAMETER, DIAMETER);
+        y = y+3;
+        ellipse(z, HEIGHT*4/5, DIAMETER, DIAMETER);
+        z = z+4;
+    }
+
+    private void paintWhite() {
+    }
+
+
+}
+
